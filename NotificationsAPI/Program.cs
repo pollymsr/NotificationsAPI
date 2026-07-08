62c2daba0d22491d7a00d6b698e74fac63fbe3e0
Pollyana Manuela Silva Rocha <pollyana.rocha@goop.com.br>
2026-07-07 21:10:20 -0300

    refactor: limpeza de codigo, remocao de comentarios e adicao de gitignore

--- NotificationsAPI/Program.cs
+++ NotificationsAPI/Program.cs
@@ -3,8 +3,6 @@ using FiapCloudGames.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -28,7 +26,6 @@ builder.Services.AddMassTransit(x =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
